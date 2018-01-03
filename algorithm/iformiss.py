13083a130531659cac8e4a0d20ebd4da188ce829
Tadesse Zemicheal <tadesse.habte@gmail.com>
2018-01-03 15:55:17 -0800

    ifor added

--- algorithm/iformiss.py
+++ algorithm/iformiss.py
@@ -1,5 +1,28 @@
 import pyad as pft
 
+## Build Forest using bagged features.
+
+class iFor(object):
+    def __init__(self):
+        self.ntree = None
+    def train(self, train_x):
+        nrow, ncol = train_x.shape
+        forest = {}
+        for ntree in self.ntree:
+            features = feature_bag(ncol)
+            tree = pft.Tree(train_x[,features])
+            forest.append({tree:tree, features: features})
+        self.forest = forest
+    def score(self, test_x):
+        """
+        Build the depth using the trained forest.
+        :param test_x:
+        :return:
+        """
+        for tree, features in self.forest:
+            depth
+
+
 def train_forest(train_x, check_mv=False, tree_size=1):
     ff = pft.IsolationForest(train_x, ntree=100 * tree_size, nsample=512)
     return ff
