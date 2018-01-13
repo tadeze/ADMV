8d60b2857458f6f9bf520440249cca58029c4277
Tad seldovia <tadeze@live.com>
2018-01-12 23:35:50 -0800

    lof added

--- testlocally.py
+++ testlocally.py
@@ -4,15 +4,16 @@ from missvalueinjector import MissingValueInjector
 from mainexperiment import algo_miss_features, random_miss_prop
 import pandas as pd
 def test():
-    df = pd.read_csv("yeast_1.csv")
-    train_data = df.ix[:,1:].as_matrix()
+    file_name = "/nfs/guille/bugid/adams/ifTadesse/missingdata/experiments/anomaly/shuttle_1v23567/fullsamples/shuttle_1v23567_1.csv"
+    df = pd.read_csv(file_name)
+    train_data = df.ix[:,1:].as_matrix().astype(np.float64)
     train_lbl = map(int, df.ix[:, 0] == "anomaly")
     miss_colmn = range(0,5)
 
-    # result = algo_miss_features(
-    #     train_data, train_lbl, "yeast.csv", miss_colmn,'IFOR')
-    result = random_miss_prop(
-        train_data, train_lbl, 'BIFOR',miss_colmn,)
+    result = algo_miss_features(
+         train_data, train_lbl, miss_colmn,'LODA')
+    #result = random_miss_prop(
+    #     train_data, train_lbl, miss_colmn,'IFOR')
     print result
 def test_cell_injector():
     w = np.random.randn(10,5)
@@ -20,7 +21,6 @@ def test_cell_injector():
     ix = ad_in.inject_missing_in_random_cell(w,0.3)
     print w
     print ix
-
 
 
 if __name__ == '__main__':
--- util/common.py
+++ util/common.py
@@ -5,25 +5,94 @@ import sklearn.metrics as mt
 ## MISSING VALUE FLAGES
 MISSING_VALUE = -9999.0
 missing_value_per = 0.1
+#NA = np.nan
 NA = np.nan
-
 def rbind(r1, r2):
     if r1 is None:
         return np.copy(r2)
     return np.append(r1, r2, axis=0)
 
+def nrow(x):
+    if len(x.shape) == 2:
+        return x.shape[0]
+    return None
+def ncol(x):
+    if len(x.shape) == 2:
+        return x.shape[1]
+    return None
 
-def cbind(c1, c2):
-    if len(c1.shape) == 1 and len(c2.shape) == 1:
-        mat = np.empty(shape=(len(c1), 2))
-        m[:, 0] = c1
-        m[:, 1] = c2
-        return mat
+def rbind(m1, m2):
+    if m1 is None:
+        return np.copy(m2)
+    return np.append(m1, m2, axis=0)
+
+def cbind(m1, m2):
+    if len(m1.shape) == 1 and len(m2.shape) == 1:
+        if len(m1) == len(m2):
+            mat = np.empty(shape=(len(m1), 2))
+            mat[:, 0] = m1
+            mat[:, 1] = m2
+            return mat
+        else:
+            raise ValueError("length of arrays differ: (%d, %d)" % (len(m1), len(m2)))
+    return np.append(m1, m2, axis=1)
+def matrix(d, nrow=None, ncol=None, byrow=False):
+    """Returns the data as a 2-D matrix
+
+    A copy of the same matrix will be returned if input data dimensions are
+    same as output data dimensions. Else, a new matrix will be created
+    and returned.
+
+    Example:
+        d = np.reshape(range(12), (6, 2))
+        matrix(d[0:2, :], nrow=2, byrow=True)
+
+    Args:
+        d:
+        nrow:
+        ncol:
+        byrow:
+
+    Returns: np.ndarray
+    """
+    if byrow:
+        # fill by row...in python 'C' fills by the last axis
+        # therefore, data gets populated one-row at a time
+        order = 'C'
     else:
-        raise ValueError("length of different")
-    return np.append(c1, c1, axis=1)
+        # fill by column...in python 'F' fills by the first axis
+        # therefore, data gets populated one-column at a time
+        order = 'F'
+    if len(d.shape) == 2:
+        d_rows, d_cols = d.shape
+    elif len(d.shape) == 1:
+        d_rows, d_cols = (1, d.shape[0])
+    else:
+        raise ValueError("Dimensions more than 2 are not supported")
+    if nrow is not None and ncol is None:
+        ncol = int(d_rows * d_cols / float(nrow))
+    elif ncol is not None and nrow is None:
+        nrow = int(d_rows * d_cols / float(ncol))
+    if len(d.shape) == 2 and d_rows == nrow and d_cols == ncol:
+        return d.copy()
+    if not d_rows * d_cols == nrow * ncol:
+        raise ValueError("input dimensions (%d, %d) not compatible with output dimensions (%d, %d)" %
+                         (d_rows, d_cols, nrow, ncol))
+    if isinstance(d, csr_matrix):
+        return d.reshape((nrow, ncol), order=order)
+    else:
+        return np.reshape(d, (nrow, ncol), order=order)
 
+def order(x, decreasing=False):
+    if decreasing:
+        return np.argsort(-x)
+    else:
+        return np.argsort(x)
 
+def sample(x, n):
+    shuffle = np.array(x)
+    np.random.shuffle(shuffle)
+    return shuffle[0:n]
 def metric(label, score):
     auc = mt.roc_auc_score(label, score)
     ap = mt.average_precision_score(label, score)
