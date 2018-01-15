1bc5cea436786846e30b1fdd485073538fb432ad
Tadesse Zemicheal <tadesse.habte@gmail.com>
2018-01-14 18:02:07 -0800

    lof added

--- algorithm/lof.py
+++ algorithm/lof.py
@@ -51,41 +51,74 @@ class BaggedLOF(object):
             self.models.append({"lof": lof, "cols":cols})
             self.model_features[lof_model,cols] = 1
 
-    def score(self, test_df, check_miss=True):
+    def _score(self, X, check_miss):
+
+        if X.ndim>1:
+            raise  ValueError("Should be applied on 1-d data.")
         self.num_models_used = []
         self.check_miss = check_miss
 #        return super(BaggedIForest, self).score(test_df)
 
         if self.check_miss:
-            miss_column = get_miss_features(test_df)
+            miss_column = get_miss_features(X)
         else:
             miss_column = []
         all_scores = []
         non_missing_models = available_models(self.model_features, miss_column)
-        maskcol = np.ones(test_df.shape, dtype=bool)
+        maskcol = np.ones(X.shape, dtype=bool)
         maskcol[miss_column] = False
         if len(non_missing_models) < 1:
             return 0.0
-
         for model_inst in non_missing_models:
             lof = self.models[model_inst]
-            sliced_data = test_df[lof["cols"]]
+            sliced_data = X[lof["cols"]].reshape([1, len(lof["cols"])])
             all_scores.append(lof["lof"].score(sliced_data))
         self.num_models_used.append(len(non_missing_models))
-        return np.mean(all_scores)
+        return -1*np.mean(all_scores)  ## Make anomlies with larger values.
+    def score(self, test_df, check_miss=False):
+        """
+        This can be applied to either vector or matrix vector.
+        :param test_df: nXd numpy matrix.
+        :param check_miss:
+        :return:
+        """
+        assert isinstance(test_df, np.ndarray)
+        if test_df.ndim<2:
+            return self._score(test_df, check_miss=check_miss)
+        return [ self._score(row, check_miss) for row in test_df]
+
 
 
 if __name__ == '__main__':
 
     ll = LOF()
+    import pandas as pd
+    df = pd.read_csv('/home/tadeze/projects/missingvalue/datasets/anomaly/yeast/fullsamples/yeast_1.csv')
+    train_data = df.ix[:, 1:].as_matrix().astype(np.float64)
+    # train_lbl = df.ix[:,0] #
+    train_lbl = map(int, df.ix[:, 0] == "anomaly")
     w = np.random.randn(100,5)
     w[1,2] = 20
     w[5,0]  = -30
+    lbl = np.zeros([1, w.shape[0]])
+    lbl[:,[1,5]] = 1
+    #print lbl[0]
     ff = ll.train(w)
-    print ff.score(w)
+    #print ff.score(w)
     #import sklearn
     #print sklearn.__version__
-    lb = BaggedLOF()
-    lb.train(w)
+    lb = BaggedLOF(num_model=40)
+    lb.train(train_data)
     #print lb.models
-    lb.score(w, False)
+    sc =  lb.score(train_data)
+    train_data[0,1] = np.nan
+    train_data[1,3] = np.nan
+    print sc[0:3]
+
+    sx = lb.score(train_data[0:3,],True)
+    print sx
+    print "Assume not handled"
+    sx = lb.score(train_data[0:3, ], False)
+    print sx
+
+    print metric(train_lbl,sc )
